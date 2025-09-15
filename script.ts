349a112e16b38b76d597260d017c3dc0d051818b
Edriane <doidoiwey@gmail.com>
2025-09-15 22:08:22 +0800

    file(script.ts): add group type

--- script.ts
+++ script.ts
@@ -4,4 +4,12 @@ enum MovieGenre {
     THRILLER = "thriller",
     HORROR = "horror",
     SCIFI = "scifi"
-}
+};
+
+type Movie = {
+    title: string;
+    year: Date;
+    director: string;
+    genre: MovieGenre;
+    print(): void;
+};
