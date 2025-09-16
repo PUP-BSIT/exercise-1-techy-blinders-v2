90313e78b1fbbe6ae36c4ac5af175e2b4d064318
consultado-kirby <kirbyconsultado@gmail.com>
2025-09-15 22:51:52 -0700

    file(script.ts): add my object type

--- script.js
+++ script.js
@@ -9,11 +9,21 @@ var MovieGenre;
 ;
 var mosquito = {
     title: "The Maze Runner",
-    year: new Date("2014"),
+    year: 2014,
     director: "Wes Ball",
     genre: MovieGenre.SCIFI,
     print: function () {
-        console.log("\n                Title: ".concat(this.title, ",\n                Year: (").concat(this.year.getFullYear(), "),\n                Directed by: ").concat(this.director, ",\n                Genre: ").concat(this.genre));
+        console.log("\n                Title: ".concat(this.title, ",\n                Year: ").concat(this.year, ",\n                Directed by: ").concat(this.director, ",\n                Genre: ").concat(this.genre));
+    }
+};
+var consultado = {
+    title: "John Wick",
+    year: 2014,
+    director: "Chad Stahelski",
+    genre: MovieGenre.ACTION,
+    print: function () {
+        console.log("\n                Title: ".concat(this.title, ",\n                Year: ").concat(this.year, ",\n                Directed by: ").concat(this.director, ",\n                Genre: ").concat(this.genre));
     }
 };
 mosquito.print();
+consultado.print();
